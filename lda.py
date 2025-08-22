a6067c3097bdc3a74a03e9f85c53fc985cc54554
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2025-08-22 17:05:26 +0530

    Create

---
+++ lda.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+#dataset
+X, y = make_classification(n_samples = 1000, n_features = 8, n_informative = 8, n_redundant = 0, random_state = 2)
+
+model = LinearDiscriminantAnalysis()
+cv = RepeatedStratifiedKFold(n_splits = 10, n_repeats = 3, random_state = 2)
+
+grid = dict()
+grid['solver'] = ['svd', 'lsqr', 'eigen']
+
+search = GridSearchCV(model, grid, scoring = 'accuracy', cv = cv, n_jobs = -1)
+results = search.fit(X, y)
+
+print('Mean Accuracy: %.4f' % results.best_score_)
+print('Classes:')
+print(results.classes_)
+
+print('---')
+
+row = [0.1277, -3.6440, -2.2326, 1.8211, 1.7546, 0.1243, 1.0339, 2.3582]   #new data
+yhat = search.predict([row]) 
+print('Predicted class of new datapoint:')
+print(yhat) 
